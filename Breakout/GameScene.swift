5d66f81d1759c509dcae6c3c0358451137122e12
Ethan <ethan.scott2@icloud.com>
2024-03-21 09:31:24 -0500

    Created ball object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -12,10 +12,16 @@ class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    var ball = SKShapeNode()
     
     override func didMove(to view: SKView) {
         createBackground()
+        resetGame()
         }
+    
+    func resetGame() {
+        makeBall()
+    }
     
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
@@ -31,6 +37,25 @@ class GameScene: SKScene {
             starsBackground.run(moveForever)
         }
     }
+    
+    func makeBall() {
+        ball.removeFromParent()
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.strokeColor = .black
+        ball.fillColor = .yellow
+        ball.name = "ball"
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        ball.physicsBody?.isDynamic = false
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        ball.physicsBody?.friction = 0
+        ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.restitution = 1
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        addChild(ball)
+    }
+    
     }
     
   
